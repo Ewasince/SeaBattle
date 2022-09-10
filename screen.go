ef1a4a3af88552c0885bba2574b4444b06eabf77
Ewasince <ewasince@gmail.com>
2022-09-10 13:32:13 +0300

    moved create ship function

--- screen.go
+++ screen.go
@@ -30,19 +30,29 @@ func (sc Screen) setShips() {
 	helperScreen := Screen{}
 	for _, ship := range SHIPS {
 		for i := 0; i < ship.count; i++ {
-			x := rand.Intn(FSize)
-			y := rand.Intn(FSize)
-			if helperScreen.field[x][y] == Void {
-				dir := DIRECTIONS[rand.Intn(4)]
-				gipoTiles := make([][2]int, 0, 4)
-				for j := 0; j < ship.size; j++ {
-					newX := x + dir.x*j
-					newY := y + dir.y*j
-					gipoTiles = append(gipoTiles, [2]int{newX, newY})
-
-					fmt.Println(gipoTiles)
-				}
-			}
+			fmt.Println(helperScreen)
 		}
 	}
+}
+
+func generateShip(size int, screen *Screen, helper *Screen) {
+	x := rand.Intn(FSize)
+	y := rand.Intn(FSize)
+	direct := DIRECTIONS[rand.Intn(4)]
+	gipoTiles := make([][2]int, 0, 4)
+	for j := 0; j < ship.size; j++ {
+		newX := x + direct.x*j
+		newY := y + direct.y*j
+
+		if (*helper).field[newX][newX] != Void {
+			flag := false
+
+		}
+		gipoTiles = append(gipoTiles, [2]int{newX, newY})
+	}
+
+	for _, tile_ := range gipoTiles {
+
+	}
+
 }
